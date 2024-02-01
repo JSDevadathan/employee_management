294de8a4c8deaaf637f861c74268aa271b4540ad
JSDevadathan <jsdevadathan@gmail.com>
2024-02-01 13:13:16 +0530

    Testing Done

--- src/test/java/com/example/Employee/Management/System/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/Employee/Management/System/controller/EmployeeControllerTest.java
@@ -2,10 +2,8 @@ package com.example.Employee.Management.System.controller;
 
 import com.example.Employee.Management.System.contract.request.EmployeeRequest;
 import com.example.Employee.Management.System.contract.response.EmployeeResponse;
-import com.example.Employee.Management.System.model.Employee;
 import com.example.Employee.Management.System.service.EmployeeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,6 +19,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.ArrayList;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -65,11 +63,11 @@ public class EmployeeControllerTest {
     void getEmployeesByDepartment() throws Exception {
         when(employeeService.getEmployeesByDepartment(Mockito.<String>any()))
                 .thenReturn(new ArrayList<>());
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/employees/department/{department}","department");
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/employees/department/{department}", "department");
         MockMvcBuilders.standaloneSetup(employeeController)
                 .build()
                 .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
                 .andExpect(MockMvcResultMatchers.content().string("[]"));
     }
--- src/test/java/com/example/Employee/Management/System/service/EmployeeServiceTest.java
+++ src/test/java/com/example/Employee/Management/System/service/EmployeeServiceTest.java
@@ -1,7 +1,59 @@
 package com.example.Employee.Management.System.service;
 
+import com.example.Employee.Management.System.contract.request.EmployeeRequest;
+import com.example.Employee.Management.System.contract.response.EmployeeResponse;
+import com.example.Employee.Management.System.model.Employee;
+import com.example.Employee.Management.System.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class EmployeeServiceTest {
+    @MockBean
+    private EmployeeRepository employeeRepository;
+
+    @MockBean
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Test
+    void testEmployees() {
+        when(employeeRepository.save(Mockito.<Employee>any())).thenReturn(new Employee());
+        when(modelMapper.map(Mockito.<Object>any(), Mockito.<Class<EmployeeResponse>>any()))
+                .thenReturn(EmployeeResponse.builder().id(1L).name("Name").email("email@gmail.com").department("dept").build());
+        employeeService.createEmployee(new EmployeeRequest("Name", "Email", "dept"));
+        verify(modelMapper).map(Mockito.<Object>any(), Mockito.<Class<EmployeeResponse>>any());
+        verify(employeeRepository).save(Mockito.<Employee>any());
+    }
+
+    @Test
+    void getEmployeesByDepartment(){
+        when(employeeRepository.findByDepartment(Mockito.any())).thenReturn(new ArrayList<>());
+        Employee employee = Employee.builder()
+                .id(1L)
+                .name("Name")
+                .department("dept")
+                .email("email@gmail.com")
+                .build();
+
+        List<EmployeeResponse> actualEmployeeByDepartment = employeeService.getEmployeesByDepartment("dept");
+        verify(employeeRepository).findByDepartment(Mockito.any());
+        assertTrue(actualEmployeeByDepartment.isEmpty());
+    }
 }
